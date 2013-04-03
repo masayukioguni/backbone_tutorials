c04aa8340fc79ec6d99fcf84867dacbcf3413890
masayuki_oguni <masayuki_oguni@dwango.co.jp>
2013-04-03 21:38:20 +0900

    いろいろ追加

--- 3_check/3.js
+++ 3_check/3.js
@@ -1,8 +1,8 @@
 (function($) {
     var Item = Backbone.Model.extend({
     	defaults: {
-    		part1: 'hello',
-    		part2: 'world'
+    		part1: 'hecclo',
+    		part2: 'wocld'
     	}
     });
 
@@ -14,24 +14,30 @@
 	  el: $('body'),
 
 	  events: {
-	  	'click button#add': 'addItem'
+	  	'click button#add': 'addItem',
+      'click button#write': 'writeItem'
 	  },
 	  initialize: function() {
-	    _.bindAll(this,'render','addItem','appendItem');
+	    _.bindAll(this,'render','addItem','appendItem','writeItem');
 
 	    this.collection = new List();
 	    this.collection.bind('add', this.appendItem);
+
+      var item = new Item();
+      item.set({
+        part1: 'aaaaa',
+        part2: 'bbbbb'
+      })
+
+      this.collection.add(item);
 
 	    this.counter = 0;
 	    this.render();
       },
       render: function(){
-      	var self = this;
       	$(this.el).append("<button id='add'>Add list item</button>");
+        $(this.el).append("<button id='write'>write list item</button>");
         $(this.el).append("<ul></ul>");
-        _(this.collection.models).each(function(item) {
-          self.appendItem(item);
-        }, this);
       },
       addItem: function(){
         this.counter++;
@@ -39,11 +45,17 @@
         item.set({
         	part2: item.get('part2') + this.counter
         })
-        this.collection.add(item)
+        this.collection.add(item,{silent: true});
       },
       appendItem: function(item){
         $('ul', this.el).append("<li>"+item.get('part1')+" "+item.get('part2')+"</li>");
-      }
+      },
+      writeItem: function(){
+        var self = this;
+        _(this.collection.models).each(function(item) {
+          self.appendItem(item);
+        }, this);
+      },
 	});
    var listView = new ListView();      
 })(jQuery);
